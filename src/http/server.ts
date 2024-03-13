38795edfe2de41b176e16a7c2836069fd3500a3b
Jefferson Silva <jeffsilva01.dev@gmail.com>
2024-03-12 23:12:34 -0300

    feat: Added restaurant registration controller
    The registration requires sending name, email, and phone to register a user with manager role.
    For restaurant registration, restaurantName and description must be sent, with description being an optional parameter.

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,43 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000)
+const app = new Elysia().post(
+  '/restaurants',
+  async ({ body, set }) => {
+    const { restaurantName, managerName, email, description, phone } = body
 
-console.log(
-  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
+    const [manager] = await db
+      .insert(users)
+      .values({
+        email,
+        phone,
+        name: managerName,
+        role: 'manager',
+      })
+      .returning({ id: users.id })
+
+    await db.insert(restaurants).values({
+      name: restaurantName,
+      description,
+      managerId: manager.id,
+    })
+
+    set.status = 204
+  },
+  {
+    body: t.Object({
+      restaurantName: t.String(),
+      managerName: t.String(),
+      email: t.String({ format: 'email' }),
+      description: t.Optional(t.String()),
+      phone: t.String(),
+    }),
+  },
 )
+
+app.listen(3000, () => {
+  console.log(
+    `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
+  )
+})

b19cf1e4d51b284a746d15ab83b436079955118a
Jefferson Silva <jeffsilva01.dev@gmail.com>
2024-03-12 22:42:47 -0300

    feat: Added ESLint configuration along with eslint-plugin for Drizzle
    Added ESLint configuration file (.eslintrc.js) and installed eslint-plugin for Drizzle to enforce code quality standards.

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,55 +1,44 @@
-import chalk from "chalk";
-import { faker } from "@faker-js/faker"
-import { db } from "./connection";
-import { restaurants, users } from "./schema";
+/* eslint-disable drizzle/enforce-delete-with-where */
+import chalk from 'chalk'
+import { faker } from '@faker-js/faker'
+import { db } from './connection'
+import { restaurants, users } from './schema'
 
-/**
-  * Reset database
-  */
 await db.delete(users)
 await db.delete(restaurants)
 
 console.log(chalk.greenBright('✅ Database reset!'))
 
-/**
-  * Create custumers 
-  */
-
 await db.insert(users).values([
   {
     name: faker.person.fullName(),
     email: faker.internet.email(),
-    role: "customer"
+    role: 'customer',
   },
   {
     name: faker.person.fullName(),
     email: faker.internet.email(),
-    role: "customer"
-  }
+    role: 'customer',
+  },
 ])
 
 console.log(chalk.greenBright('✅ Create customers!'))
 
-/**
-  * Create Manager 
-  */
-
-const [manager] = await db.insert(users).values({
-  name: faker.person.fullName(),
-  email: "jeffsilva01.dev@gmail.com",
-  role: "manager"
-}).returning({ id: users.id })
+const [manager] = await db
+  .insert(users)
+  .values({
+    name: faker.person.fullName(),
+    email: 'jeffsilva01.dev@gmail.com',
+    role: 'manager',
+  })
+  .returning({ id: users.id })
 
 console.log(chalk.greenBright('✅ Create manager!'))
-
-/**
-  * Create restaurant
-  */
 
 await db.insert(restaurants).values({
   managerId: manager.id,
   name: faker.company.name(),
-  description: faker.lorem.paragraph()
+  description: faker.lorem.paragraph(),
 })
 
 console.log(chalk.greenBright('✅ Create restaurant!'))

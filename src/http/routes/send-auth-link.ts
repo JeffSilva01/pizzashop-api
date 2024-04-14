48f61a08fb45c7ef24d57e1570f93233625e2a18
Jefferson Silva <jeffsilva01.dev@gamil.com>
2024-04-13 21:37:53 -0300

    feature: apply create auth link use case

--- src/http/routes/send-auth-link.ts
+++ src/http/routes/send-auth-link.ts
@@ -1,34 +1,20 @@
 import { Elysia, t } from 'elysia'
 
 import { env } from '../../env'
-import { db } from '../../db/connection'
-import { authLinks } from '../../db/schema'
-import { createId } from '@paralleldrive/cuid2'
 import queue from '../../lib/queue'
+import { makeCreateAuthenticateCode } from '../../use-cases/factories/make-create-authenticate-code'
 
 export const sendAuthLink = new Elysia().post(
   '/authenticate',
   async ({ body, set }) => {
     const { email } = body
 
-    const user = await db.query.users.findFirst({
-      where: (users, { eq }) => eq(users.email, email),
-    })
-
-    if (!user) {
-      throw new Error('User not found.')
-    }
-
-    const authLinkCode = createId()
-
-    await db.insert(authLinks).values({
-      userId: user.id,
-      code: authLinkCode,
-    })
+    const createAuthLinksUseCase = makeCreateAuthenticateCode()
+    const { code } = await createAuthLinksUseCase.execute({ userEmail: email })
 
     const authLink = new URL('/auth-links/authenticate', env.API_BASE_URL)
 
-    authLink.searchParams.set('code', authLinkCode)
+    authLink.searchParams.set('code', code)
     authLink.searchParams.set('redirect', env.AUTH_REDIRECT_URL)
 
     queue.add('registrationMail', { authLink, email })
